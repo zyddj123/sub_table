dfa8c9bfa10f75619b7bddb560281971d1209aa5
zyddj123 <zyddj123@163.com>
2019-01-15 17:44:37 +0800

    coding

--- application/controller/Demo1Controller.php
+++ application/controller/Demo1Controller.php
@@ -8,14 +8,11 @@ if (!defined('BASECHECK')){
 
 /**
  * 演示Demo Lesson 1，包括：
- * 控制器命名方式
- * 控制器默认加载函数
- * 控制器默认入口函数
- * 自定义控制器函数
+ * 分表 插入、查询 数据
  * 
  * @package		comnide
  * @author			B.I.T
- * @copyright		Copyright (c) 2016 - 2017.
+ * @copyright		Copyright (c) 2017 - 2019.
  * @license
  * @link
  * @since				Version 1.17
@@ -23,27 +20,39 @@ if (!defined('BASECHECK')){
  */
 class Demo1Controller extends Controller{
 	private $db='';
-	/**
-	 * 默认加载函数
-	 * 控制器在初始化时会运行此函数
-	 */
+
 	protected function _init(){
 		$this->db = GetDB();
 	}
 	
-	/**
-	 * 默认入口
-	 */
 	function run(){
+
+	}
+
+	//分表插入数据
+	function insertData(){
+		$month = date("Y-m");
+		$table = 'data'.$month;
+		$data = array('name'=>'zhangsan','visit_time'=>date('Y-m-d'));
+		$flag = $this->db->query("show tables like '{$table}'");
+		// var_dump($flag);
+		if($flag){
+			//有此表则插入数据
+			$this->db->insert($table,$data);
+		}else{
+			//没有此表则创建此表
+
+		}
 		
 	}
 
-	function insertData(){
-
-	}
-
+	//分表读取数据
 	function readData(){
-
+		$t_start = "2018-12-15";
+		$t_end = "2019-01-13";
+		$sql = 'select * from `data2018-12` WHERE visit_time BETWEEN "2018-12-10" AND "2019-01-20" UNION select * from `data2019-01` WHERE visit_time BETWEEN "2018-12-10" AND "2019-01-20"';
+		$res = $this->db->getAll($sql);
+		var_dump($res);
 	}
 	
 }
